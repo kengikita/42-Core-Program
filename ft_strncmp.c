c171d217f6656e93f43a47925d0da0de9d849ca3
Kengi <angrierbird@gmail.com>
2021-08-04 01:26:07 +0800

    strncmp function

---
+++ ft_strncmp.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+	int				num;
+
+	i = n;
+	while (i > 0 && *s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+			break ;
+		i--;
+		s1++;
+		s2++;
+	}
+	if (i == 0)
+		num = 0;
+	else
+		num = *(unsigned char *)s1 - *(unsigned char *)s2;
+	return (num);
+}
