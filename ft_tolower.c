e503ffe78bf5a5226ec7dbf545d6ea7d3df38229
Kengi <angrierbird@gmail.com>
2021-08-04 01:19:37 +0800

    Changes uppercase letter to lowercase

---
+++ ft_tolower.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+char	*ft_tolower(char *str)
+{
+	int	i;
+	int	num;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		num = str[i];
+		if (num >= 65 && num <= 90)
+		{
+			str[i] = num + 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
+/*#include <stdio.h>
+int main(void)
+{
+	char *a;
+	char b[] = "ABCDEZ";
+
+	a = ft_tolower(b);
+	printf("%s",a);
+}*/
