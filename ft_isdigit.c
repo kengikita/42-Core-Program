37e583369aaa4893c23df7c63feea0ad00d7400f
Kengi <angrierbird@gmail.com>
2021-08-04 00:04:33 +0800

    Function to test whether is it a string of numbers

---
+++ ft_isdigit.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+int	ft_isdigit(char *str)
+{
+	int	i;
+	int	t;
+	int	num;
+
+	i = 0;
+	if (str[i] == '\0')
+	{
+		t = 1;
+	}
+	else
+	{
+		while (str[i] != '\0')
+		{
+			num = str[i];
+			if (num >= 48 && num <= 57)
+				t = 1;
+			else
+			{
+				t = 0;
+				break ;
+			}
+			i++;
+		}
+	}
+	return (t);
+}
