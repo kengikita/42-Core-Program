4feaa41b053a9b33b8b1fb50639372cc7aa9d190
Kengi <angrierbird@gmail.com>
2021-08-04 01:03:32 +0800

    Turns lowercase alphabet into uppercase

---
+++ ft_toupper.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+char	*ft_toupper(char *str)
+{
+	int	i;
+	int	num;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		num = str[i];
+		if (num >= 97 && num <= 122)
+		{
+			str[i] = num - 32;
+		}
+		i++;
+	}
+	return (str);
+}
