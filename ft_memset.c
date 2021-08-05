962383d42dc45c01954707985715e7fccf2c2d94
Kengi <angrierbird@gmail.com>
2021-08-05 22:06:54 +0800

    Memset function

---
+++ ft_memset.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+void	*memset(void *str, int c, unsigned int n)
+{
+	unsigned char	*p;
+
+	p = (unsigned char *)str;
+	while (n > 0)
+	{
+		*ptr = (unsigned char)c;
+		ptr++;
+		n--;
+	}
+	return (str);
+}
