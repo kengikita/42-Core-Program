3c787dc7965f5afd2dea61a23dcb23b138b45597
Kengi <angrierbird@gmail.com>
2021-08-07 17:18:53 +0800

    Function strchr

---
+++ ft_strchr.c
@@ -0,0 +1,26 @@
+#include "libft.h"
+
+char	*strchr(char *s, int c)
+{
+	char	*ptr;
+
+	ptr = s;
+	while (*s != c)
+	{
+		ptr++;
+		s++;
+	}
+	return (ptr);
+}
+
+/*int main () {
+   const char str[] = "http://www.tutorialspoint.com";
+   const char ch = '.';
+   char *ret;
+
+   ret = ft_strchr(str, ch);
+
+   printf("String after |%c| is - |%s|\n", ch, ret);
+   
+   return(0);
+}*/
