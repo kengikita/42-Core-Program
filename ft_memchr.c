bc5551abfa6eb2eb90c5be6b8d71fface73e8ea2
Kengi <angrierbird@gmail.com>
2021-08-07 18:35:51 +0800

    Memchr function

---
+++ ft_memchr.c
@@ -0,0 +1,28 @@
+#include "libft.h"
+
+void	*ft_memchr(const void *str, int c, size_t n)
+{
+	unsigned char	*ptr;
+
+	ptr = (unsigned char *)str;
+	while (n > 0)
+	{
+		if (*ptr == (unsigned char)c)
+			return (*ptr);
+		n--;
+		ptr++;
+	}
+	return (0);
+}
+
+/*int main () {
+   const char str[] = "http://www.tutorialspoint.com";
+   const char ch = '*';
+   char *ret;
+
+   ret = memchr(str, ch, 5);
+
+   printf("String after |%c| is - |%s|\n", ch, ret);
+
+   return(0);
+}*/
