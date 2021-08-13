6536d93a4cdd4be92722146e14537b430eaf3e84
Kengi <angrierbird@gmail.com>
2021-08-13 19:21:14 +0800

    Bug Fix

--- ft_memmove.c
+++ ft_memmove.c
@@ -4,23 +4,20 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char		*s1;
 	const unsigned char	*s2;
-	size_t				i;
 
-	i = 1;
 	s1 = (unsigned char *)dest;
 	s2 = (unsigned char *)src;
+	if (dest == src || n == 0)
+		return (dest);
 	if (s2 < s1)
 	{
-		while (i <= n)
-		{
-			s1[n - i] = s2[n - i];
-			i++;
-		}
+		while (--n)
+			s1[n] = s2[n];
 	}
 	else
 	{
 		while (n-- > 0)
-			*(s1++) = *(s2++);
+			*s1++ = *s2++;
 	}
 	return (s1);
 }
