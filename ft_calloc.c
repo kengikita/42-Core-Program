0eee6aa8e914af027289edfa488c38ddec523c12
Kengi <angrierbird@gmail.com>
2021-08-14 00:41:33 +0800

    Bug Fix

--- ft_calloc.c
+++ ft_calloc.c
@@ -9,9 +9,8 @@ void	*ft_calloc(size_t nelem, size_t size)
 		nelem = 1;
 		size = 1;
 	}
-	ptr = (void *)malloc(nelem * size);
-	if (!ptr)
-		return (0);
-	bzero(ptr, nelem);
+	ptr = (char *)malloc(nelem * size);
+	if (ptr)
+		bzero(ptr, nelem);
 	return (ptr);
 }
