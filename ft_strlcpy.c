9fd17cf5e5aa27dfcd2d3484700dd6080d52aa45
Kengi <angrierbird@gmail.com>
2021-08-16 00:56:42 +0800

    test

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,17 +1,23 @@
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize)
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
 	size_t	i;
+	size_t	count;
 
-	if (!dstsize)
+	if (!src)
+		return (0);
+	if (!size)
 		return (ft_strlen(src));
 	i = 0;
-	while (src[i] && i < dstsize - 1)
+	count = 0;
+	while (src[i] && i < (size - 1))
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
-	return (ft_strlen(src));
+	while (src[count])
+		count++;
+	return (count);
 }
