ee95d87f94ce31859ab5ada4f89748b7cd859bc3
Kengi <angrierbird@gmail.com>
2021-08-15 19:38:02 +0800

    Bug Fix

--- ft_striteri.c
+++ ft_striteri.c
@@ -4,10 +4,13 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	unsigned int	i;
 
-	i = 0;
-	while (s[i])
+	if (s)
 	{
-		(*f)(i, &s[i]);
-		i++;
+		i = 0;
+		while (s[i])
+		{
+			(*f)(i, &s[i]);
+			i++;
+		}
 	}
 }
