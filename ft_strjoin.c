87156343e46d3a5c2bde4bb29c0159f4ede669c6
Kengi <angrierbird@gmail.com>
2021-08-15 19:46:27 +0800

    Bug Fix

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -2,27 +2,27 @@
 
 char	*ft_strjoin(const char *s1, const char *s2)
 {
-	size_t	sizes1;
-	size_t	sizes2;
+	size_t	sizes;
 	char	*join;
 	int		i;
 
 	i = 0;
-	sizes1 = ft_strlen(s1);
-	sizes2 = ft_strlen(s2);
-	join = malloc(sizeof(char *) * (sizes1 + sizes2 + 1));
-	if (!join)
-		return (NULL);
-	while (*s1 != '\0')
+	sizes = ft_strlen(s1) + ft_strlen(s2);
+	join = malloc(sizeof(char *) * (sizes + 1));
+	if (join)
 	{
-		join[i] = *(s1++);
-		i++;
+		while (*s1 != '\0')
+		{
+			join[i] = *(s1++);
+			i++;
+		}
+		while (*s2 != '\0')
+		{
+			join[i] = *(s2++);
+			i++;
+		}
+		join[i] = '\0';
+		return (join);
 	}
-	while (*s2 != '\0')
-	{
-		join[i] = *(s2++);
-		i++;
-	}
-	join[i] = '\0';
-	return (join);
+	return (NULL);
 }
