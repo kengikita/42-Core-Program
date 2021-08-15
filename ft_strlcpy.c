dea4c95af093888ceabdb03ab6aa22649b19fe6a
Kengi <angrierbird@gmail.com>
2021-08-16 00:58:53 +0800

    Type casted the output to size_t because strlen returns int

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,23 +1,17 @@
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize)
 {
 	size_t	i;
-	size_t	count;
 
-	if (!src)
-		return (0);
-	if (!size)
-		return (ft_strlen(src));
+	if (!dstsize)
+		return ((size_t)ft_strlen(src));
 	i = 0;
-	count = 0;
-	while (src[i] && i < (size - 1))
+	while (src[i] && i < dstsize - 1)
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
-	while (src[count])
-		count++;
-	return (count);
+	return ((size_t)ft_strlen(src));
 }
