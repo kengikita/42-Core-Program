7822ccf83dca9fc5f6c0dd32e53ef13e70d12c70
Kengi <angrierbird@gmail.com>
2021-08-15 19:35:49 +0800

    Bug Fix

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -5,16 +5,20 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char		*s1;
 	const unsigned char	*s2;
 
-	s1 = (unsigned char *)dest;
-	s2 = (unsigned char *)src;
-	while (n > 0)
+	if (dest || src)
 	{
-		*s1 = *s2;
-		s1++;
-		s2++;
-		n--;
+		s1 = (unsigned char *)dest;
+		s2 = (unsigned char *)src;
+		while (n > 0)
+		{
+			*s1 = *s2;
+			s1++;
+			s2++;
+			n--;
+		}
+		return (dest);
 	}
-	return (dest);
+	return (NULL);
 }
 
 /*#include <stdio.h>
