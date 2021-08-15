0d7c83eec58cf0273105ff7b421444a45b44aa68
Kengi <angrierbird@gmail.com>
2021-08-15 19:53:36 +0800

    Bug Fix

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -11,7 +11,7 @@ char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
 	str = malloc(sizeof(char *) * (ft_strlen(s) + 1));
 	if (!str)
 		return (NULL);
-	while (s[i] != '\0')
+	while (s[i])
 	{
 		str[i] = (*f)(i, s[i]);
 		i++;
